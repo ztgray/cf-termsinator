481202c2612b839399fe52fddd7f09a1660ef50e
ztgray <ztgray@gmail.com>
2012-10-23 18:10:26 -0700

    logging and content type

--- app.js
+++ app.js
@@ -9,20 +9,26 @@ var port = process.env.PORT || 3000;
 app.use(express.bodyParser());
 
 app.post('/api/accounts', function(req, res) {
+	console.log(req.body);
+	
 	var result = {
 		account_id: -1,
 		status: "error"
 	};
 	
+	res.type('application/json');
 	res.send(JSON.stringify(result));
 });
 
 app.post('/api/domains', function(req, res) {
+	console.log(req.body);
+
 	var result = {
 		domain_id: -1,
 		status: "error"
 	};
 	
+	res.type('application/json');
 	res.send(JSON.stringify(result));	
 });
 
